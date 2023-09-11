56cb0b13afc854252478561ad5dcc23463f54ba8
Anhdo2308 <144682905+Anhdo2308@users.noreply.github.com>
2023-09-11 19:07:34 +0700

    Learn C
    Learn C at Aptech

---
+++ EPC_DAY1/Vidu01.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	printf("Hello C Programming. Drilled to engage a new thing!");
+	getch();
+}
---
+++ EPC_DAY1/Vidu02_tinh tong.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int a,b;
+	printf("Hello C Programming. Drilled to engage a new thing!");
+	printf("Nhap a=")
+	getch();
+}
